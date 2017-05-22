4df93f2ff318722f494208e6d982138c5901a19e
Scott Livingston <s.livingst@gmail.com>
2017-05-22 13:10:01 -0600

    higher kinded types

--- src/main/scala/types.scala
+++ src/main/scala/types.scala
@@ -3,7 +3,7 @@ package lambdaconf.types
 import matryoshka._
 import monocle._
 import scalaz._
-
+import scala.language.higherKinds
 import Scalaz._
 
 object exercise1 {
@@ -18,15 +18,39 @@ object exercise1 {
 }
 
 object exercise2 {
-  final case class Box[A](/* ??? */)
+  final case class Box[A](value: A)
+
+  val fb: Box[Int] = ???
+  val fb = Box(1)
+
+
+  val f: List[Int] = ???
+  // List : (X: Type) => Type
+  // Int : *
+  // List : * => *
+  // Either : [*, *] => *
+  // Map : [*, *] => *
+
+  // val f: Either = ???  // ERROR!
+  // val f: Either[String] = ???  // ERROR!
+  // val f: Either[,Int] = ???  // ERROR!
+  val f2: Either[String, Int] = ??? // OK!
+
+  def identity[A](x: A): A = x // A has kind * and x has type A
+
+  //def foo[E: [*, *] => *, A: *, B: *](value: E[A, B]) = ??? // i want to feed either to this but i can cause scala doesnt like it :(
+  def foo[E[_, _], A, B](value: E[A, B]) = ??? //so i do this instead!
+
 }
 
 object exercise3 {
-  // 1. scala.collection.List
-  // 2. F[_, _]
-  // 3. Option
-  // 4. Int
-  // 5. T[_[_], _]
+  // 1. scala.collection.List // * => *
+  // 2. F[_, _] // [*, *] => *
+  // 3. Option // * => *
+  // 4. Int // *
+  // 5. T[_[_], _] // [* => *, *] => *
+                   // or
+                   // (* => *) => * => *
 }
 
 object exercise4 {
