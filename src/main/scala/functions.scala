a62c6e8382df4b074c37aebb4b84d4c1fb2f2017
Scott Livingston <s.livingst@gmail.com>
2017-05-22 09:46:45 -0600

    working on function combinators

--- src/main/scala/functions.scala
+++ src/main/scala/functions.scala
@@ -17,8 +17,44 @@ object exercise2 {
 
 object exercise3 {
   type Parser[A] = String => Either[String, (String, A)]
+  type ErrorMsg = String
+  type Input = String
 
-  def or[A](left: Parser[A], right: Parser[A]): Parser[A] = ???
+  def fail[A](err: ErrorMsg): Parser[A] = (_: Input) => Left(err)
+
+  def anyChar: Parser[Char] = (input: Input) =>
+    if (input.length == 0)
+      Left("expected char, found EOF")
+    else
+      Right((input.drop(1), input.charAt(0)))
+
+  def satisfies[A](p: Parser[A])(f: A => Boolean): Parser[A] =
+    (input: String) => p(input) match {
+      case Left(err) => Left(err)
+      case Right((input, a)) =>
+        if (f(a)) Right((input, a))
+        else Left("predicate failed")
+    }
+
+  val parseLeftCBracket: Parser[Char] = satisfies(anyChar)(_ == '{')
+  val parseLeftSBracket: Parser[Char] = satisfies(anyChar)(_ == '{')
+
+  val parseLeftAnyBracket: Parser[Char] = or(parseLeftCBracket, parseLeftSBracket)
+
+  def or[A](left: Parser[A], right: Parser[A]): Parser[A] =
+    (input: Input) => left(input) match {
+      case Left(_) => right(input)
+      case x => x
+    }
+
+  def then[A, B](first: Parser[A], second: Parser[B]): Parser[(A, B)] =
+//    (input) => first(input) match {
+//      case Right(i, a) => second(i) match {
+//        case Right(t) => ???
+//        case Left(e) => ???
+//      }
+//      case l => l
+//    }
 }
 
 object exercise4 {
