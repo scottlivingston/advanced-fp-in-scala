c53d74047bde204d1d67d6290c95198ec6ccca09
Scott Livingston <s.livingst@gmail.com>
2017-05-22 15:09:52 -0600

    type classes

--- src/main/scala/typeclasses.scala
+++ src/main/scala/typeclasses.scala
@@ -2,11 +2,41 @@ package lambdaconf.typeclasses
 
 import matryoshka._
 import monocle._
-import scalaz._
 
+import scalaz._
 import Scalaz._
+import scala.util.Try
 
 object exercise1 {
+
+  /** Laws
+    * forAll[A](a => read(show(a)) == Some(a))
+    */
+  trait Debuggable[A] {
+    def show(a: A): String
+    def read(s: String): Option[A]
+  }
+
+  object Debuggable {
+    def apply[A](implicit d: Debuggable[A]) = d
+
+    implicit val DebuggableString = new Debuggable[String] {
+      def show(a: String) = a
+      def read(s: String) = Some(s)
+    }
+
+    implicit val DebuggableInt = new Debuggable[Int] {
+      def show(a: Int) = a.toString
+      def read(s: String) = Try(s.toInt).toOption
+    }
+  }
+
+  implicit class DebuggagleSyntax[A](a: A) {
+    def show(implicit d: Debuggable[A]) = d.show(a)
+  }
+
+
+
   sealed trait PathLike[A] {
     // ???
   }
